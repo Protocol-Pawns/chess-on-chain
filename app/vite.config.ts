a7658c38aa79534927040fccdbbbc6284e33e471
Mario Reder <mario.reder@pm.me>
2026-06-07 22:48:16 +0200

    fix build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [UnoCSS(), sveltekit(), nodePolyfills({ include: ['buffer'] })],
+  plugins: [UnoCSS(), sveltekit()],
   ssr: {
     external: ['util', 'is-my-json-valid', 'generate-function', 'jsonpointer']
   }
